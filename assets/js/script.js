64b66595639c653f4e348313994597e229e92346
agostinoluca <agostino.luca89@gmail.com>
2023-11-15 19:48:16 +0100

    add script, add style

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+/*
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito23
+
+Strumenti:
+-prompt
+-const
+-getElementById
+-innerHtml
+*/
+
+// ask name
+const UserFirstName = prompt(`What's your name?`)
+
+// ask last name
+const UserLastName = prompt(`What's your last name?`)
+
+// ask favourite colour
+const FavoriteColor = prompt(`What's your favourite colour?`)
+
+// get element by id username
+const UserName = document.getElementById('username')
+
+// welcome message + innerhtml with result name + last name
+UserName.innerHTML = `Ciao ${UserFirstName+` `+UserLastName}, benvenuto nel tuo generatore di password.`
+
+// get element by id password
+const password = document.getElementById(`password`)
+
+// create password
+password.innerHTML = `${UserFirstName+UserLastName+FavoriteColor+23}`
